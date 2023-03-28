58d51fbb0a6c2938942c94347d4e590a14b1e626
suttipong tanmuangma <suttipong-gk@outlook.com>
2023-03-28 21:57:28 +0700

    Add history query to show payment transaction.

--- BankingSystem_Challenge/Controllers/HomeController.cs
+++ BankingSystem_Challenge/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Security.Claims;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BankingSystem_Challenge.Controllers
 {
@@ -55,8 +56,40 @@ namespace BankingSystem_Challenge.Controllers
         }
 
         public IActionResult TransactionHistory()
-        { 
-            return View();
+        {
+            var userID = "";
+            if (_httpContextAccessor != null)
+            {
+                userID = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            }
+
+            var myAccount = _context.Accounts.Where(i => i.CustomerId == userID).FirstOrDefault();
+
+            var myTransaction = from t in _context.Transactions
+                                join a1 in _context.Accounts on t.FromAccountId equals a1.AccountId
+                                join a2 in _context.Accounts on t.ToIban equals a2.Iban
+                                join u in _context.Users on a1.CustomerId equals u.Id
+                                join u2 in _context.Users on a2.CustomerId equals u2.Id
+                                where u.Id == userID || u2.Id == userID
+                              select new TransactionListViewModel
+                              {
+                                  TransactionType = t.FromAccountId == myAccount.AccountId ? "Transfer To" : "Received From",
+                                  AccountName = t.FromAccountId == myAccount.AccountId ? u2.first_name + ' ' + u2.last_name : u.first_name + ' ' + u.last_name,
+                                  Iban = t.FromAccountId == myAccount.AccountId ? a2.Iban : a1.Iban,
+                                  Amount = t.Amount,
+                                  DateExcute = t.DateExecuted
+                              };
+
+            var myTransactionList = myTransaction.ToList();
+
+            TransactionHistoryViewModel transactionHistoryViewModel = new TransactionHistoryViewModel()
+            {
+                balance = myAccount.Balance,
+                iban = myAccount.Iban,
+                transactionListViewModels = myTransactionList,
+            };
+
+            return View(transactionHistoryViewModel);
         }
 
         public IActionResult Privacy()
--- BankingSystem_Challenge/ViewModels/TransactionListViewModel.cs
+++ BankingSystem_Challenge/ViewModels/TransactionListViewModel.cs
@@ -2,9 +2,10 @@
 {
     public class TransactionListViewModel
     {
-        public string TransactionType { get; set; }
-        public string AccountName { get; set; }
-        public double Amount { get; set; }
-        public DateTime DateExcute { get; set; }
+        public string? TransactionType { get; set; }
+        public string? AccountName { get; set; }
+        public string? Iban { get; set; }
+        public double? Amount { get; set; }
+        public DateTime? DateExcute { get; set; }
     }
 }
